4cc04f77e1b8e56cde196f14ccf89a3f460f7f36
Jake Nichols <yummyfunnybunny13@gmail.com>
2024-03-12 09:02:32 -0600

    edit about info. fix responsive width issues with showcase and timeline.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,17 +13,22 @@ function About() {
           <p className='section__text'>
             I have spent my entire working life in customer-facing eviornments. Whether it be in front-desk hotel
             reception, assistant management in the restaurant & entertainment sector, or emergency dispatching in the
-            electrical sector.{' '}
+            electrical sector, and most recently, coordinating with drivers, dispatchers, and customers in the freight
+            brokerage sector. I've thrived in high-paced work environments that demand constant adaptation.
           </p>
           {/* <div className='section__highlight'> */}
           <p className='section__text section__highlight section__highlight--light'>
-            I take pride in solving problems for people.
+            I've thrived in high-paced work environments that demand constant adaptation.
           </p>
-          {/* </div> */}
           <p className='section__text'>
             I earned my BSBA in Supply Chain Logistics Management and took electives in advanced excel and data
-            analytics, and now strive to add software to my arsenal of skillsets. I hope to combine my logistics
-            education, customer service experience, and software capabilities to provide a unique service to the world!
+            analytics. I've most recently added software engineering to my skillset and am eager to provide a unique set
+            of skills in the software sector. With a proven track record of people skills and coordination, I am excited
+            to continue carving my niche in this world. There's nothing more rewarding than working hard and solving
+            problems for other people.
+          </p>
+          <p className='section__text section__highlight section__highlight--light'>
+            There's nothing more rewarding than working hard and solving problems for other people.
           </p>
         </div>
       </div>
