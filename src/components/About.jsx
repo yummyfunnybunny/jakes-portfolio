52a39ba4b04f5a3e9d5aaffe6cbb5fc6492b9e40
Jake Nichols <yummyfunnybunny13@gmail.com>
2024-03-12 09:27:40 -0600

    fix about section typos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
         <div className='about__content'>
           <h1 className='section__title'>Welcome</h1>
           <p className='section__text'>
-            I have spent my entire working life in customer-facing eviornments. Whether it be in front-desk hotel
+            I have spent my entire working life in customer-facing environments. Whether it be in front-desk hotel
             reception, assistant management in the restaurant & entertainment sector, or emergency dispatching in the
             electrical sector, and most recently, coordinating with drivers, dispatchers, and customers in the freight
             brokerage sector. I've thrived in high-paced work environments that demand constant adaptation.
@@ -22,10 +22,10 @@ function About() {
           </p>
           <p className='section__text'>
             I earned my BSBA in Supply Chain Logistics Management and took electives in advanced excel and data
-            analytics. I've most recently added software engineering to my skillset and am eager to provide a unique set
-            of skills in the software sector. With a proven track record of people skills and coordination, I am excited
-            to continue carving my niche in this world. There's nothing more rewarding than working hard and solving
-            problems for other people.
+            analytics. I've most recently added software engineering to my skill-set and am eager to provide a unique
+            set of skills in the software sector. With a proven track record of people skills and coordination, I am
+            excited to continue carving my niche in this world. There's nothing more rewarding than working hard and
+            solving problems for other people.
           </p>
           <p className='section__text section__highlight section__highlight--light'>
             There's nothing more rewarding than working hard and solving problems for other people.
